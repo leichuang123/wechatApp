c263a47fb41b14b30c97df3d1764147b8d19004e
jikang <jk@rmlx.cn>
2019-01-23 08:13:36 +0000

    小程序联系我们页面信息改为从数据库获取
    git-svn-id: http://192.168.88.7/svn/pc/weapp/branches/weapp_dev@5167 ddfa41ad-a237-462f-a13d-ab6c0720e9a2

--- src/pages/settings/contact-us.js
+++ src/pages/settings/contact-us.js
@@ -1,19 +1,48 @@
+import api from '../../utils/api';
+import {
+    showLoading
+} from '../../utils/util';
+const app = getApp();
+
 Page({
     data: {
-        phoneNumber: '027-59613798',
+        AUTH_TYPE_OEM:1,
+        contactInfo: {},
     },
     /**
      * 拨打电话
      */
-    call: function (e) {
+    call: function(e) {
+        if (!this.data.contactInfo.service_hotline) {
+            return;
+        }
         wx.makePhoneCall({
-            phoneNumber: this.data.phoneNumber,
-            success: function (res) {
+            phoneNumber: this.data.contactInfo.service_hotline,
+            success: function(res) {
                 console.log('拨打成功');
             },
-            fail: function (res) {
+            fail: function(res) {
                 console.log('拨打失败');
             }
         })
+    },
+    getContactInfo: function() {
+        const authType = app.globalData.extConfig.auth_type;
+        const params = {
+            type: authType === this.data.AUTH_TYPE_OEM ? 'oem' : 'merchant',
+            id: app.globalData.extConfig.auth_related_id || 1,
+        };
+        showLoading();
+        api.get('oem-public/get-other-setting', params).then(res => {
+            wx.hideLoading();
+            if (res.errcode === 0) {
+                this.setData({
+                    contactInfo: res.data,
+                });
+            }
+        });
+    },
+    onLoad: function() {
+        this.getContactInfo();
     }
 })
