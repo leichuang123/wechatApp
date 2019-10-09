5f21af4b3cc2b08d63ef961077ca4501933494c4
leichuang <lc@rml.cn>
2019-10-09 06:50:13 +0000

    修复字段错误
    git-svn-id: http://192.168.88.7/svn/pc/weapp/branches/weapp_dev@8684 ddfa41ad-a237-462f-a13d-ab6c0720e9a2

--- src/pages/promotion/index.js
+++ src/pages/promotion/index.js
@@ -35,7 +35,7 @@ Page({
             latitude: '',
             area_code: '',
             auth_type: app.globalData.extConfig.auth_type || 0,
-            auth_related_id: app.globalData.extConfig.auth_type || 0,
+            auth_related_id: app.globalData.extConfig.auth_related_id || 0,
             page: 1
         }
     },
@@ -155,7 +155,7 @@ Page({
             'form.longitude': locationInfo.longitude,
             'form.area_code': !!selectedCity ? selectedCity.code : locationInfo.adcode,
             'form.auth_type': app.globalData.extConfig.auth_type || 0,
-            'form.auth_related_id': app.globalData.extConfig.auth_type || 0,
+            'form.auth_related_id': app.globalData.extConfig.auth_related_id || 0,
             sliderLeft: (app.globalData.windowWidth / this.data.tabs.length - this.data.sliderWidth) / 2,
             sliderOffset: (app.globalData.windowWidth / this.data.tabs.length) * this.data.activeIndex
         });
