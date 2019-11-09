7d30d7e91104edb303f99619c53114bc5acd297c
liuhao <lh@rmlx.cn>
2019-11-09 00:55:34 +0000

    上传文件接口的网络协议改为HTTPS
    git-svn-id: http://192.168.88.7/svn/pc/weapp/branches/weapp_dev@9150 ddfa41ad-a237-462f-a13d-ab6c0720e9a2

--- src/config.js
+++ src/config.js
@@ -2,17 +2,19 @@
  * 小程序配置文件
  */
 // const host = 'bms-local.com'; //local
-const host = 't17.hb400.net'; //staging
-//const host = 'sh.huobanyc.com'; //production
+// const host = 't8.hb400.net'; //staging
+const host = 'sh.huobanyc.com'; //hbyc
+// const host = 'new.p-oneclub.com'; //zdmc
 
 const config = {
     // host: 'http://bms-local.com/', //local
-    host: 'http://t17.hb400.net/', //staging
-    //host: 'https://sh.huobanyc.com/', //production
+    // host: 'http://t8.hb400.net/', //staging
+  host: 'https://sh.huobanyc.com/', //hbyc
+  // host: 'https://new.p-oneclub.com/', //zdmc
 
     // 上传文件接口
     // uploadFileUrl: `http://${host}/weapp/upload`,
-    uploadFileUrl: `http://${host}/weapp/upload`, //product
+    uploadFileUrl: `https://${host}/weapp/upload`, //product
     // 腾讯地图的key
     key: 'SLSBZ-BYRKG-NLYQ3-ILT6E-7OZFS-UAF4I'
 };
