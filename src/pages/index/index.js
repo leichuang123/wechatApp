e354c9e4f48ad424c8749e3df1adb9c92492c95f
leichuang <lc@rmlx.com>
2020-06-15 15:32:36 +0800

    优化小程序定位问题

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -20,7 +20,8 @@ Page({
         autoplay: true,
         interval: 5000,
         duration: 1000,
-        weappList:[]
+        weappList:[],
+        fromList:false
     },
     //获取banner
     getBannerList:function(merchant_id){
@@ -236,25 +237,24 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-        //首页首次进入
-        if (options.from !== 'list') {
-            this.getLocation();
-            return;
-        }
         //从选择门店选择门店后进入
         if (options.from == 'list') {
-            let bmsWeappStoreInfo = wx.getStorageSync('bmsWeappStoreInfo');
-            if (!bmsWeappStoreInfo) {
-                this.getLocation();
-                return;
-            }
             this.setData({
-                bmsWeappStoreInfo: bmsWeappStoreInfo
+                fromList:true,
             });
         }
     },
     onShow: function() {
         this.initData();
         wx.showTabBar();
+        let bmsWeappStoreInfo = wx.getStorageSync('bmsWeappStoreInfo');
+        //没有缓存定位信息，不是从选择门店选择门店后进入
+        if (!bmsWeappStoreInfo && !this.data.fromList) {
+            this.getLocation();
+            return;
+        }
+        this.setData({
+            bmsWeappStoreInfo: bmsWeappStoreInfo
+        });
     }
 });
